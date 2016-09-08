e1a35077a9cca82e9eb498c53cabe59ca355ed33
hukuuu <hukuuu@gmail.com>
2016-09-08 15:10:33 +0300

    game session creation

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var sock = new SockJS('http://localhost:9999/echo');
 sock.onopen = function() {
   window.socket = sock
   console.log('gogo')
+  state.mode = MODES.CHOOSE_NAME
+  update()
 };
 sock.onmessage = function(e) {
   console.log('[receive]', e.data);
@@ -14,15 +16,21 @@ sock.onclose = function() {
 
 var chooseNameSection = $('#choose-name-section')
 var chooseSessionSection = $('#choose-session-section')
+var loadingSection = $('#loading')
+var lobbySection = $('#lobby')
 var sessions = $('#sessions')
+var lobbyPlayers = $('#lobby-players')
+
 
 var MODES = {
+  LOADING: 'loading',
   CHOOSE_NAME: 'choose-name',
   CHOOSE_SESSION: 'choose-session'
 }
 
 var state = {
-  mode: MODES.CHOOSE_NAME
+  mode: MODES.LOADING,
+  sessionId: ''
 }
 
 
@@ -36,6 +44,28 @@ function init() {
   $('#session-button')
     .on('click', createSession)
 
+  lobbyPlayers
+    .on('click', getSlot(''))
+  $('#blue-tell-slot')
+    .on('click', getSlot('blue-tell'))
+  $('#blue-guess-slot')
+    .on('click', getSlot('blue-guess'))
+  $('#red-tell-slot')
+    .on('click', getSlot('red-tell'))
+  $('#red-guess-slot')
+    .on('click', getSlot('red-guess'))
+
+  update()
+
+}
+
+function getSlot(slot) {
+  return function() {
+    sock.send(message('getSlot', {
+      sessionId: state.session.id,
+      slot: slot
+    }))
+  }
 }
 
 function createSession() {
@@ -54,12 +84,10 @@ function chooseName() {
 function handleMessage(msg) {
   switch (msg.type) {
     case 'sessions':
-      if (!!msg.value.length)
-        sessions.empty()
-        .append(msg.value.map(function(session) {
-            return '<li id="' + session.id + '">' + session.name + '</li>'
-          })
-          .join(''))
+      listSessions(msg.value)
+      break;
+    case 'sessionPlayers':
+      listSessionPlayers(msg.value)
       break;
     default:
       console.log('couldnt handle message: ' + JSON.stringify(msg))
@@ -67,19 +95,67 @@ function handleMessage(msg) {
   }
 }
 
+function listSessions(s) {
+  sessions.empty()
+    .append(s.map(function(session) {
+        return '<li id="' + session.id + '">' + session.name + '</li>'
+      })
+      .join(''))
+
+  sessions.children()
+    .on('click', function() {
+      var el = $(this)
+      var session = {
+        id: el.attr('id'),
+        name: el.text()
+      }
+
+      sock.send(message('joinSession', session.id))
+      state.session = session
+      state.mode = MODES.LOBBY
+      update()
+    })
+
+}
+
+function listSessionPlayers(sessionPlayers) {
+  var list = lobbyPlayers.find('ul')
+  list.empty()
+  $('.slots .row div h1:nth-child(2)')
+    .text('[empty]')
+  sessionPlayers.forEach(function(player) {
+    if (player.slot) {
+      $('#' + player.slot + '-name')
+        .text(player.name)
+    } else {
+      list.append('<li>' + player.name + '</li>')
+    }
+  })
+
+}
+
 function update() {
 
   switch (state.mode) {
 
+    case MODES.LOADING:
+      hideAll()
+      loadingSection.show()
+      break;
+
     case MODES.CHOOSE_NAME:
+      hideAll()
       chooseNameSection.show()
-      chooseSessionSection.hide()
       break;
 
     case MODES.CHOOSE_SESSION:
+      hideAll()
       chooseSessionSection.show()
-      chooseNameSection.hide()
       sock.send(message('getSessions'))
+      break;
+
+    case MODES.LOBBY:
+      showLobby()
       break;
 
     default:
@@ -88,6 +164,20 @@ function update() {
 
 }
 
+function showLobby() {
+  hideAll()
+  lobbySection.show()
+  $('#lobby-name')
+    .text('Game ' + state.session.name)
+  sock.send(message('getSessionPlayers', state.session.id))
+}
+
+function hideAll() {
+  loadingSection.hide()
+  chooseNameSection.hide()
+  chooseSessionSection.hide()
+  lobbySection.hide()
+}
 
 function message(type, value) {
   var msg = JSON.stringify({
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const util = require('util')
 
 const players = []
 const sessions = []
+const connections = []
 
 const server = http.createServer();
 echo.installHandlers(server, {
@@ -18,45 +19,73 @@ server.listen(9999, '0.0.0.0');
 echo.on('connection', function(conn) {
 
   let player = {
-    id: uuid.v1(),
-    conn: conn
+    id: uuid.v1()
   }
+
+  connections.push({
+    id: player.id,
+    conn: conn
+  })
 
   players.push(player)
 
-
-  player.conn.on('data', function(message) {
-    handleMessage(player, JSON.parse(message))
+  conn.on('data', function(message) {
+    console.log('[receive] ' + message)
+    handleMessage(player, conn, JSON.parse(message))
   });
 
-  player.conn.on('close', function() {
+  conn.on('close', function() {
+    console.log('close???')
+    console.log(player.currentSessionId)
+    var session = findSession(player.currentSessionId)
+    session.players.splice(session.players.indexOf(player), 1)
+    broadcastSession(session, (message('sessionPlayers', session.players)))
+
     players.splice(players.indexOf(player), 1)
+    connections.splice(connections.indexOf(connections.filter(function(connection) {
+      return connection.id === player.id
+    })[0]), 1)
   });
+
 });
 
 
 
-function handleMessage(player, msg) {
+function handleMessage(player, conn, msg) {
   switch (msg.type) {
     case 'setName':
-      console.log('set player name: ' + message.value)
       player.name = msg.value
       break;
 
     case 'getSessions':
-      player.conn.write(message('sessions', sessions))
+      conn.write(message('sessions', sessions))
       break;
+
     case 'createSession':
-      console.log(player.name)
       var session = {
         id: uuid.v1(),
         name: 'session' + (sessions.length + 1),
-        players: [player.name]
+        players: []
       }
       sessions.push(session)
-      console.log(sessions)
-      player.conn.write(message('sessions', sessions))
+      broadcast(connections, message('sessions', sessions))
       break;
+
+    case 'joinSession':
+      player.currentSessionId = msg.value
+      var session = findSession(msg.value)
+      session.players.push(player)
+      broadcastSession(session, (message('sessionPlayers', session.players)))
+      break;
+
+    case 'getSlot':
+      getSlot(conn, player, findSession(msg.value.sessionId), msg.value.slot)
+      break;
+
+    // case 'getSessionPlayers':
+    //   conn.write(message('sessionPlayers', findSession(msg.value)
+    //     .players))
+    //   break;
 
     default:
       console.log('unknown message: ' + JSON.stringify(msg, null, 4))
@@ -66,11 +95,47 @@ function handleMessage(player, msg) {
 
 }
 
+function getSlot(conn, player, session, slot) {
+  var slotTaken = session.players.filter(function(player) {
+      return player.slot === slot
+    })
+    .length > 0
+  if (!slotTaken || !slot) {
+    player.slot = slot
+  }
+  broadcastSession(session, (message('sessionPlayers', session.players)))
+}
+
+function findSession(id) {
+  return sessions.filter(function(session) {
+    return session.id === id
+  })[0]
+}
+
 function message(type, value) {
-  var msg = JSON.stringify(JSON.parse(util.inspect({
+  var msg = JSON.stringify({
     type: type,
     value: value
-  })))
+  })
   console.log('[send] ' + msg)
   return msg
+}
+
+function broadcast(connections, message) {
+  connections.forEach(function(conn) {
+    conn.conn.write(message)
+  })
+}
+
+function broadcastSession(session, message) {
+  var ids = session.players.map(function(player) {
+    return player.id
+  })
+  console.log(players)
+  var conns = connections.filter(function(conn) {
+    return ids.indexOf(conn.id) > -1
+  })
+  console.log(conns.length)
+
+  return broadcast(conns, message)
 }
